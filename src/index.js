53978715d7818db41f9ec932fbeb353087fa97dd
AlokHimself <alokhiself@gmail.com>
2021-07-13 20:25:17 +0100

    js function lab

---
+++ src/index.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/**
+ * Problem #1: You Rock
+ *
+ * Define a function named `youRock` that accepts a persons name
+ * as a string argument and returns a string using that name.
+ *
+ * Example:
+ * > You rock Sam!
+ *
+ * If the function does not receives a string as a parameter
+ * then it should return `undefined`
+ */
+
+// ANSWER
+
+
+/**
+ * Problem #2: Square
+ *
+ * Define a `square` function that accepts a number argument
+ * and returns that number multiplied by itself.
+ *
+ * Example:
+ * square(2) // 4
+ */
+
+// ANSWER
+
+
+/**
+ * Problem #3: Calculator
+ *
+ * Write a function that will add, subtract, multiply and divide
+ * two numbers and return the result.
+ *
+ * Examples:
+ * calculator(1, 2, "add") // 3
+ * calculator(1, 2, "subtract") // -1
+ * calculator(1, 2, "divide") // .5
+ * calculator(1, 2, "multiply") // 2
+ * calculator(1, 2, "something else") // Calculator can only add, subtract, multiply and divide
+ * calculator("Dog", 2, "add") // Calculator only accepts numbers
+ */
+
+// ANSWER
+
+
+/**
+ * Problem #4: Fizz Buzz
+ *
+ * Define a function that accepts a number argument.
+ *
+ * - If it's a multiple of 3, return "Fizz"
+ * - If it's a multiple of 5, return "Buzz"
+ * - If it's a multiple of both, return "Fizz Buzz"
+ * - Otherwise, return the number
+ *
+ * Example:
+ * fizzBuzz(3) // Fizz
+ * fizzBuzz(15) // Fizz Buzz
+ * fizzBuzz(7) // 7
+ */
+
+// ANSWER
